7fd1fe6bb669c84e5639c34495b35481c7949261
Abarri <officiel.abarri@gmail.com>
2017-01-24 12:12:46 +0000

    fixed seeds

--- database/seeds/HomeSectionsTableSeeder.php
+++ database/seeds/HomeSectionsTableSeeder.php
@@ -10,6 +10,6 @@ class HomeSectionsTableSeeder extends Seeder
         DB::table('home_sections')->delete();
         HomeSection::create(['id' => 1, 'language_id' => '2',
          'title' => 'Section example 1','slug'=>'section-example-1',
-         'content'=>'&lt;section class="callaction"&gt;<br /> &lt;div class="container"&gt;<br /> &lt;div class="row"&gt;<br /> &lt;div class="col-lg-8"&gt;<br /> &lt;div class="cta-text"&gt;<br /> &lt;h2&gt;Awesome site template &lt;span&gt;corporate&lt;/span&gt; business&lt;/h2&gt;<br /> &lt;p&gt; Etiam adipiscing, justo quis feugiat.Suspendisse eu erat quam. Vivamus porttitor eros quis nisi lacinia sed interdum&lt;/p&gt;<br /> &lt;/div&gt;<br /> &lt;/div&gt;<br /> &lt;div class="col-lg-4"&gt;<br /> &lt;div class="cta-btn"&gt;<br /> &lt;a href="#" class="btn btn-theme btn-lg"&gt;Grab it now &lt;i class="fa fa-angle-right"&gt;&lt;/i&gt;&lt;/a&gt;<br /> &lt;/div&gt;<br /> &lt;/div&gt; <br /> &lt;/div&gt;<br /> &lt;/div&gt;<br /> &lt;/section&gt;']);
+         'content'=>"<section class=\"callaction\">\n<div class=\"container\">\n<div class=\"row\">\n<div class=\"col-lg-8\">\n<div class=\"cta-text\">\n<h2>Awesome site template corporate business</h2>\n<p>Etiam adipiscing, justo quis feugiat.Suspendisse eu erat quam. Vivamus porttitor eros quis nisi lacinia sed interdum</p>\n</div>\n</div>\n<div class=\"col-lg-4\">\n<div class=\"cta-btn\"><a class=\"btn btn-theme btn-lg\" href=\"#\">Grab it now </a></div>\n</div>\n</div>\n</div>\n</section>"]);
     }
 }
