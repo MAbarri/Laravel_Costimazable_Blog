04d00b5a2aaba3e94fb1846b9fb9f97863ed106c
Abarri <officiel.abarri@gmail.com>
2017-01-31 17:14:22 +0000

    added warning for parent

--- resources/views/admin/pages/edit.blade.php
+++ resources/views/admin/pages/edit.blade.php
@@ -4,8 +4,11 @@
     {!! form($form) !!}
     @include('partials.admin.tinymce')
     <br>
-    <div class="alert alert-danger">
-							<strong>Note that</strong> if you wanna fill your content with HTML containing images, you hsould click on the "insert/edit image", and upload the image you need, and close the box, and then include the next line of code adding to it your styling
+    <div class="alert alert-warning">
+							<strong>Note that :</strong> If you wanna fill your content with HTML containing images, you hsould click on the "insert/edit image", and upload the image you need, and close the box, and then include the next line of code adding to it your styling.
               <p><br /> &lt;img src="/files/your file name" alt="image title"&gt;</p>
+						</div>
+    <div class="alert alert-warning">
+							<strong>Note that :</strong> If you select an option for the "Parent page", This page will be inaccessible from the main menu, to make it accessible again please contact your developer, or unfill the "Parent page" field.
 						</div>
 @endsection
